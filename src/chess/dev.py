790c0bc911cde79bcc37774ec8ef0f2fdc988bd6
weisscoding <30901564+weisscoding@users.noreply.github.com>
2018-02-12 10:10:56 -0800

    update on notes

--- src/chess/dev.py
+++ src/chess/dev.py
@@ -1,36 +1,36 @@
 import numpy as np
 
-empty = 0
+EMPTY = 0
 
-bPawn = -1
-bRook = -2
-bKnight = -3
-bBishop = -4
-bQueen = -5
-bKing = -6
+INVERSE = -1
 
-wPawn = 1
-wRook = 2
-wKnight = 3
-wBishop = 4
-wQueen = 5
-wKing = 6
+BLACK = -1
+WHITE = 1
+
+PAWN = 1
+ROOK = 2
+KNIGHT = 3
+BISHOP = 4
+QUEEN = 5
+KING = 6
 
 
 class Board:
-
-    __board = np.array([[2,3,4,5,6,4,3,2],
-                        [1,1,1,1,1,1,1,1],
-                        [0,0,0,0,0,0,0,0],
-                        [0,0,0,0,0,0,0,0],
-                        [0,0,0,0,0,0,0,0],
-                        [0,0,0,0,0,0,0,0],
-                        [-1,-1,-1,-1,-1,-1,-1,-1],
-                        [-2,-3,-4,-5,-6,-4,-3,-2]])
+    def __init__(self):
+        self.__board = np.array([[2, 3, 4, 5, 6, 4, 3, 2],
+                                 [1, 1, 1, 1, 1, 1, 1, 1],
+                                 [0, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, 0],
+                                 [0, 0, 0, 0, 0, 0, 0, 0],
+                                 [-1, -1, -1, -1, -1, -1, -1, -1],
+                                 [-2, -3, -4, -5, -6, -4, -3, -2]])
+        self.__movedRooks = np.array([0, 0, 0, 0])  # wRook1,wRook2, bRook1, bRook2
+        self.__movedKings = np.array([0, 0])
 
     def print(self):
-        for i in range(0,8,1):
-            for j in range(0,8,1):
+        for i in range(0, 8, 1):
+            for j in range(0, 8, 1):
                 print("%3d" % (self.__board[7 - i][j]), end='')
 
             print()
@@ -45,12 +45,30 @@ class Board:
 
         return True
 
+    # returns array of [[a1,a2,b1,b2],[a1,....],[...],]
+    def get_possible_moves(self, color):
+        pm = []
+        if color != BLACK and color != WHITE:
+            raise (ValueError, "Color specified as " + color)
+        for i in range(0,8,1):
+            for j in range(0,8,1):
+                piece = self.__board[i][j]
+                if color == BLACK and piece >= BLACK * KING and piece <= BLACK * PAWN
+                    or color == WHITE and piece >= WHITE * PAWN and piece <= BLACK * PAWN :
+                    if piece == color * PAWN:
+                        if(self.__board[i + color][j] == EMPTY):
+                            pm.append(np.array([i,j,i + color,j]))
+
+
+
+class Game:
+    def __init__(self):
+        self.board = Board()
+        self.round = 0
+        self.atTurn = BLACK
+
 
 board = Board()
 board.print()
-board.update(0, 0, 7, 7)
-board.print()
 
-val = board.update(10, 0, 7, 7)
-board.print()
-print(val)
+print(board.get_possible_moves(BLACK))
