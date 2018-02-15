5e070de63bbd9fdc57af2a27dfa7d05b81859ca2
weisscoding <30901564+weisscoding@users.noreply.github.com>
2018-02-15 15:51:26 -0800

    implemented and tested sigmoid and feedforward method

--- src/chess/recent(2018-02-13-)/NeuralNetwork.py
+++ src/chess/recent(2018-02-13-)/NeuralNetwork.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 #
@@ -17,14 +18,14 @@ import numpy as np
 
 
 class NeuralNetwork:
-    thetas = [];
-    size = [];
+    thetas = []
+    size = []
 
     def __init__(self, *args):
         if type(args[0]) is np.ndarray:
             # copy thetas from args
             for i in range(0, len(args), 1):
-                theta_is_valid = True;
+                theta_is_valid = True
                 if i > 0:
                     # valid if num rows in last matrix equals num columns in new matrix
                     # for i=0, matrix num of columns should be bigger than one
@@ -40,24 +41,49 @@ class NeuralNetwork:
                     # check for validity of the passed matrices
 
                 self.size.append(len(self.thetas[i][0]) - 1)
-            self.size.append(len(self.thetas[len(args) - 1])) # since index i accounts for neur. in layer (i - 1)
+            self.size.append(len(self.thetas[len(args) - 1]))  # since index i accounts for neur. in layer (i - 1)
 
         if type(args[0]) is int:
             # create arrays of Theta in accordance with the number of neurons per layer.
             for i in range(0, len(args) - 1, 1):
                 self.thetas.append(np.ones((args[i + 1], args[i] + 1)))
-                self.size.append(args(i))
-            self.size.append(args(len(args) - 1))  # since last index in loop only goes to len - 2
+                self.size.append(args[i])
+            self.size.append(args[len(args) - 1])  # since last index in loop only goes to len - 2
 
-    # def feed_forward(self, x):
+    def feed_forward(self, x):
+
+        activation = np.array(x)
+
+        # raising several exceptions
+        if activation.ndim != 1:
+            raise Exception("Bad format of input vector, only 1 dim supported")
+
+        if len(activation) != len(self.thetas[0][0]) - 1:
+            raise Exception("Bad format of input vector, len not according to theta matrix size")
+
+        # perform feed forward. a: activation of current layer
+
+        for theta in self.thetas:
+            activation = self.sigmoid(np.dot(theta, np.append([1], activation)))
+
+        return activation
+
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + math.exp(x))
+
+    def sigmoid_d1(self, x):
+        return self.sigmoid(x) * (1 - self.sigmoid(x))
 
 
-a = np.array([[1, 2], [3, 4], [5, 6]])
-b = np.array([[0, 0, 0, 0], [1, 1, 1, 1]])
-c = np.ones((1, 3))
+a = np.ones((1, 2))
+b = np.ones((1, 2))
 
 # print(len(c[0]))
 
-nn = NeuralNetwork(a, b, c)
+nn = NeuralNetwork(a,b)
 
-print(np.dot(a, np.array([[1,2],[4,5]])))
+activation = nn.feed_forward(np.array([1]))
+
+print(activation)
+print(nn.sigmoid(1 + nn.sigmoid(2)))
