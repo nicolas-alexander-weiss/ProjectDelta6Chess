46bbcc94643051d1718a609563b0ed0e78ad66a7
weisscoding <30901564+weisscoding@users.noreply.github.com>
2018-03-22 11:46:20 -0700

    make available

--- src/chess/recent/PlayChess_onREG.py
+++ src/chess/recent/PlayChess_onREG.py
@@ -11,7 +11,7 @@ color = {True: "white", False:" black"}
 mltpl = {True: 1, "white": 1, False: -1, "black": -1}
 
 # labels * -1 if black wins
-labels = {"draw": 0, "resign": 0.75, "mate": 5}
+labels = {"draw": -40, "resign": 75, "mate": 50}
 
 # X structure:
 # castling indicator if still possible as true/false -> 1/0
@@ -109,7 +109,78 @@ class PlayChess:
             print("GameOver, draw")
         print("rounds:", self.board.fullmove_number, "board:", self.board.fen())
 
+        # training
+        if self.train:
+            self.ai.train_clf(self.acc_training_data, self.acc_labels)
 
+    def comp_vs_human(self, own_color=True):
+        self.board = chess.Board()
+
+        self.acc_training_data = training_data_beg
+        if self.verbose:
+            self.print_board_to_file()
+        while not self.board.is_game_over():
+            if self.board.turn == own_color:
+                move = input("Please enter your move: ")
+                try:
+                    if chess.Move.from_uci(move) not in self.board.legal_moves:
+                        print("These are the legal moves: ", self.board.legal_moves)
+                        continue
+                except ValueError:
+                    continue
+                self.acc_training_data = np.vstack((self.acc_training_data, self.get_feature_vector(self.board)))
+                self.board.push_uci(move)
+            else:
+                white_at_turn = self.board.turn
+
+                possible_moves = list(self.board.legal_moves)
+
+                self.board.push(possible_moves[0])
+                best_move = possible_moves[0]
+                best_val = self.ai.clf.predict([self.get_feature_vector(self.board)])[0]
+                if self.board.is_checkmate():
+                    best_val = mltpl[white_at_turn] * 1000000
+                elif self.board.is_game_over(claim_draw=True):
+                    best_val = mltpl[white_at_turn] * -1000000
+
+                self.board.pop()
+                # print(best_val)
+                for i in range(1, len(possible_moves), 1):
+                    self.board.push(possible_moves[i])
+                    feature_vector = self.get_feature_vector(self.board)
+                    val = self.ai.clf.predict([feature_vector])[0]
+                    # print(val)
+                    if self.board.is_checkmate():
+                        val = mltpl[white_at_turn] * 1000000
+                    elif self.board.is_game_over(claim_draw=True):
+                        val = mltpl[white_at_turn] * -1000000
+
+                    if white_at_turn and val > best_val:
+                        best_val = val
+                        best_move = possible_moves[i]
+                    elif (not white_at_turn) and val < best_val:
+                        best_val = val
+                        best_move = possible_moves[i]
+                    self.board.pop()
+                    # print(val)
+
+                if white_at_turn and best_val < 0 and self.board.can_claim_draw():
+                    break
+                elif (not white_at_turn) and best_val > 0 and self.board.can_claim_draw():
+                    break
+
+                self.acc_training_data = np.vstack((self.acc_training_data, self.get_feature_vector(self.board)))
+                self.board.push(best_move)
+                # print("chosen: ", best_val)
+            if self.verbose:
+                self.print_board_to_file()
+
+        self.acc_labels = np.ones((len(self.acc_training_data), 1)) * self.get_label(self.board)
+        if self.board.is_checkmate():
+            print("GameOver, Checkmate, Winner:", not self.board.turn)
+        else:
+            print("GameOver, draw")
+        print("rounds:", self.board.fullmove_number, "board:", self.board.fen())
 
         # training
         if self.train:
@@ -147,6 +218,7 @@ class PlayChess:
         file.close()
 
 if __name__ == "__main__":
-    game = PlayChess("reg70_0.3_normalized", verbose=False)
-    for i in range(0,1,1):
+    game = PlayChess("reg70_0.3_normalized_v2", verbose=False)
+    for i in range(0, 500, 1):
         game.comp_vs_comp()
+    print("Trained on a total of", game.ai.stats[0], "board positions")
